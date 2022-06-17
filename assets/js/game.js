1ea1790867e8481e42546871b59d5c77672d5d21
Bell <jacobbrister71@gmail.com>
2022-06-17 19:01:43 -0400

    added robot variables and the ability to loop until a robot loses all health

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,13 +1,33 @@
-function fight() {
-    window.alert("The fight has begun!");
-}
 var playerName = window.prompt("What is your robot's name?");
-//fight();
+var playerHealth = 100;
+var playerAttack = 10;
 
-console.log(playerName);
+console.log(playerName, playerHealth, playerAttack);
 
-console.log("This logs a string, good for leaving yourself a message.");
+var enemyName = "Roborto";
+var enemyHealth = 50;
+var enemyAttack = 12;
 
-console.log(10+10);
+var fight = function() {
+    window.alert("Welcome to Robot Gladiators!");
+    do {
+        enemyHealth -= playerAttack;
+        if(enemyHealth <= 0) {
+            window.alert(enemyName + " has been defeated!");
+        }
+        else {
+            window.alert(enemyName + " still has " + enemyHealth + " health left.")
+        }
+        console.log(playerName + " attacked " + enemyName + ". " + enemyName + " now has " + enemyHealth + " health remaining.");
+        playerHealth -= enemyAttack;
+        if(playerHealth <= 0) {
+            window.alert(playerName + " has been defeated...");
+        }
+        else {
+            window.alert(playerName + " still has " + playerHealth + " health left.");
+        }
+        console.log(enemyName + " attacked " + playerName + ". " + playerName + " now has " + playerHealth + " health remaining.");
+    } while (playerHealth > 0 && enemyHealth > 0);
+}
 
-console.log("Our robot's name is " + playerName);
+fight();
